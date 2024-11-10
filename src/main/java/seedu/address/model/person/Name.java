de16c1e2f331a707006b5f2fc59d05271cd245a9
zienxu <zienxu.ang@gmail.com>
2024-11-10 14:00:20 +0800

    Fix name bug to include S/O but ignore this commit

--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -10,13 +10,14 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Name {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Names should only contain alphabets and spaces, and it should not be blank";
+            "Names should only contain alphabets, spaces, and the terms 's/o' or 'd/o' as part of the name. "
+                    + "Names should not be blank.";
 
     /*
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "[a-zA-Z][a-zA-Z ]*";
+    public static final String VALIDATION_REGEX = "[a-zA-Z]+( [a-zA-Z]+)*( (s/o|d/o)( [a-zA-Z]+)*)? *$";
 
     public final String fullName;
 
